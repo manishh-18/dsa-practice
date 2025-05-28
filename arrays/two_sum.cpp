171bfef6378bc3cbea960738c013df3c3b32df81
manishh-18 <manishpatsariya14@gmail.com>
2025-05-28 14:02:24 +0530

    Added Two Sum solution in arrays

---
+++ arrays/two_sum.cpp
@@ -0,0 +1,18 @@
+// Problem: Two Sum (LeetCode #1)
+// Link: https://leetcode.com/problems/two-sum/
+// Difficulty: Easy
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[j] == target - nums[i]) {
+                    return {i, j};
+                }
+            }
+        }
+        return {};
+    }
+};
