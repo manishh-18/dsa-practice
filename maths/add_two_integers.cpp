4db733e46aa702711d725a442c3040d926c3e1b9
manishh-18 <manishpatsariya14@gmail.com>
2025-06-02 10:25:29 +0530

    Added Add two Integer solution in maths

---
+++ maths/add_two_integers.cpp
@@ -0,0 +1,11 @@
+// Problem: Add Two Integers (LeetCode #2235)
+// Link: https://leetcode.com/problems/add-two-integers/
+// Difficulty: Easy
+
+
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        return num1+num2;
+    }
+};
