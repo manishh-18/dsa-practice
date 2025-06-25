315cd67cbba6e5badc8804ce5d09d0a1ebdb8033
manishh-18 <manishpatsariya14@gmail.com>
2025-06-25 14:35:42 +0530

    Added Remove Element in arrays

---
+++ arrays/remove-element.cpp
@@ -0,0 +1,18 @@
+// Problem: Remove Element (LeetCode #27)
+// Link: https://leetcode.com/problems/remove-element/
+// Difficulty: Easy
+
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+};
