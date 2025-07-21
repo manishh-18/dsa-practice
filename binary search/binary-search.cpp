68684ba4c36eb29bca71f7b84d572275962e90f8
manishh-18 <manishpatsariya14@gmail.com>
2025-07-21 21:10:18 +0530

    Added soltuion of 'Binary Search' problem in binary search

---
+++ binary search/binary-search.cpp
@@ -0,0 +1,19 @@
+// Problem: Binary Search (LeetCode #704)
+// Link: https://leetcode.com/problems/binary-search/
+// Difficulty: Easy
+
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0;
+        int high = nums.size() - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] < target) low = mid + 1;
+            else high = mid - 1;
+        }
+        return -1;
+    }
+};
