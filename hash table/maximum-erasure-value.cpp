9ca7561162f2497e6f5dd5bde310a6fc564cb86f
manishh-18 <manishpatsariya14@gmail.com>
2025-07-31 21:11:33 +0530

    Added soltuion of 'Maximum Erasure Value' problem in hash table

---
+++ hash table/maximum-erasure-value.cpp
@@ -0,0 +1,27 @@
+// Problem: Maximum Erasure Value (LeetCode #1695)
+// Link: https://leetcode.com/problems/maximum-erasure-value/
+// Difficulty: Medium
+
+
+
+class Solution {
+public:
+    static int maximumUniqueSubarray(vector<int>& nums) {
+        const int n=nums.size();
+        bitset<10001> hasX=0;
+        int ans=0, wsum=0;
+
+        for (int l=0, r=0; r<n; r++) {
+            const int x=nums[r];
+            while (hasX[x]) {
+                hasX[nums[l]]=0;
+                wsum-=nums[l];
+                l++;
+            }
+            hasX[x]=1;
+            wsum+=x;
+            ans=max(ans, wsum);
+        }
+        return ans;
+    }
+};
