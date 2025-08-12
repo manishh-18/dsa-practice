c9b293a7446c8fe7255450dff00c1700c337dd11
manishh-18 <manishpatsariya14@gmail.com>
2025-08-12 16:30:11 +0530

    Added soltuion of ' Longest Subarray With Maximum Bitwise AND' problem in bit manipulation

---
+++ bit manipulation/longest-subarray-with-maximum-bitwise-and.cpp
@@ -0,0 +1,35 @@
+// Problem: Longest Subarray With Maximum Bitwise AND (LeetCode #2419)
+// Link: https://leetcode.com/problems/longest-subarray-with-maximum-bitwise-and/
+// Difficulty: Medium
+
+
+class Solution {
+public:
+    static int longestSubarray(vector<int>& nums) {
+        const int n=nums.size();
+        int maxLen=0, xMax=0;
+        for(int r=0, l=0; r<n; r++){
+            while(r<n && nums[r]==xMax){
+                r++;
+            }
+            if (r==n || nums[r]<xMax) {
+                maxLen=max(maxLen, r-l);
+                l=r+1;
+            }
+            else {
+                xMax=nums[r];
+                maxLen=1;
+                l=r;
+            }
+        }
+        return maxLen;
+    }
+};
+
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
