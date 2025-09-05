32bf974df0de638a137ebc89bd7c8de55bf110f4
manishh-18 <manishpatsariya14@gmail.com>
2025-09-05 11:46:23 +0530

    Add soltuion of 'Number of Arithmetic Triplets' problem in arrays

---
+++ arrays/number-of-arithmetic-triplets.cpp
@@ -0,0 +1,23 @@
+// Problem: Number of Arithmetic Triplets (LeetCode #2367)
+// Link: https://leetcode.com/problems/number-of-arithmetic-triplets/
+// Difficulty: Easy
+
+
+
+class Solution {
+public:
+    int arithmeticTriplets(vector<int>& nums, int diff) {
+        int ans = 0;
+        for (int i = 0; i < nums.size() - 2; i++) {
+            for (int j = i + 1; j < nums.size() - 1; j++) {
+                for (int k = j + 1; k < nums.size(); k++) {
+                    if ((nums[j] - nums[i] == diff) &&
+                        (nums[k] - nums[j] == diff)) {
+                        ans++;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
