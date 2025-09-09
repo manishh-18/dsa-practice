fa4d4a063ea1b9547d3457481d7ec8e2be63595e
manishh-18 <manishpatsariya14@gmail.com>
2025-09-09 11:44:14 +0530

    Add soltuion of 'Strictly Palindromic Number' problem in maths

---
+++ maths/strictly-palindromic-number.cpp
@@ -0,0 +1,12 @@
+// Problem: Strictly Palindromic Number (LeetCode #2396)
+// Link: https://leetcode.com/problems/strictly-palindromic-number/
+// Difficulty: Medium
+
+
+
+class Solution {
+public:
+    bool isStrictlyPalindromic(int n) {
+        return false;
+    }
+};
