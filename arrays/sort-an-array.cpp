9940cfa619fc83aed42eb49fc9bc25ec9597bedb
manishh-18 <manishpatsariya14@gmail.com>
2025-09-13 23:20:04 +0530

    Add soltuion of 'Sort an Array' problem in arrays

---
+++ arrays/sort-an-array.cpp
@@ -0,0 +1,46 @@
+// Problem: Sort an Array (LeetCode #912)
+// Link: https://leetcode.com/problems/sort-an-array/
+// Difficulty: Medium
+
+
+
+class Solution {
+public:
+    void merge(vector<int>& nums, int low, int mid, int high) {
+        int left = low;
+        int right = mid + 1;
+        vector<int>temp;
+        while(left <= mid && right <= high){
+            if(nums[left] < nums[right]){
+                temp.push_back(nums[left]);
+                left++;
+            }
+            else{
+                temp.push_back(nums[right]);
+                right++;
+            }
+        }
+        while(left <= mid){
+            temp.push_back(nums[left]);
+            left++;
+        }
+        while(right <= high){
+            temp.push_back(nums[right]);
+            right++;
+        }
+        for(int i = low; i <= high; i++){
+            nums[i] = temp[i-low];
+        }
+    }
+    void ms(vector<int>& nums, int low, int high) {
+        if (low >= high) return;
+        int mid = (low + high) / 2;
+        ms(nums, low, mid);
+        ms(nums, mid + 1, high);
+        merge(nums, low, mid, high);
+    }
+    vector<int> sortArray(vector<int>& nums) {
+        ms(nums, 0, nums.size() - 1);
+        return nums;
+    }
+};
