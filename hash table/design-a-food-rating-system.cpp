dcfb8a9415a03db9adbbae0ed16cb7e7741ea3d0
manishh-18 <manishpatsariya14@gmail.com>
2025-09-24 16:49:17 +0530

    Add soltuion of 'Design a Food Rating System' problem in hash table

---
+++ hash table/design-a-food-rating-system.cpp
@@ -0,0 +1,29 @@
+// Problem : Design a Food Rating System (LeetCode #2353)
+// Link : https://leetcode.com/problems/design-a-food-rating-system/
+// Difficulty : Medium
+
+
+class FoodRatings {
+public:
+    unordered_map<string, pair<string,int>> foodInfo;
+    unordered_map<string, set<pair<int,string>>> cuisineMap;
+
+    FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
+        int n = foods.size();
+        for (int i = 0; i < n; i++) {
+            foodInfo[foods[i]] = {cuisines[i], ratings[i]};
+            cuisineMap[cuisines[i]].insert({-ratings[i], foods[i]});
+        }
+    }
+    
+    void changeRating(string food, int newRating) {
+        auto [cuisine, oldRating] = foodInfo[food];
+        cuisineMap[cuisine].erase({-oldRating, food});
+        cuisineMap[cuisine].insert({-newRating, food});
+        foodInfo[food].second = newRating;
+    }
+    
+    string highestRated(string cuisine) {
+        return cuisineMap[cuisine].begin()->second;
+    }
+};
