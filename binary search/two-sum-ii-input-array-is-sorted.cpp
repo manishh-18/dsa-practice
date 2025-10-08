0777279af6c82414bdc662e0199029819d2e69f0
manishh-18 <manishpatsariya14@gmail.com>
2025-10-08 10:59:14 +0530

    Add soltuion of 'Two Sum II - Input Array Is Sorted' problem in binary search

---
+++ binary search/two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,20 @@
+// Problem: Two Sum II - Input Array Is Sorted (LeetCode #167)
+// Link: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+// Difficulty: Medium
+
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int i = 0;
+        int j = numbers.size() - 1;
+        while(i < j){
+            int sum = numbers[i] + numbers[j];
+            if(sum == target) return {i+1,j+1};
+            else if(sum > target) j--;
+            else i++;
+        }
+        return {-1,-1};
+    }
+};
