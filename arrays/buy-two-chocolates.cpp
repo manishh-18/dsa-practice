30009fba9d0dd33c6cb6a29d9c504c3fd92180cb
manishh-18 <manishpatsariya14@gmail.com>
2025-10-10 11:22:39 +0530

    Add soltuion of 'Buy Two Chocolates' problem in arrays

---
+++ arrays/buy-two-chocolates.cpp
@@ -0,0 +1,14 @@
+// Problem: Buy Two Chocolates (LeetCode #2706)
+// URL: https://leetcode.com/problems/buy-two-chocolates/
+// Difficulty: Easy
+
+
+
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        sort(prices.begin(),prices.end());
+        if(prices[0] + prices[1] <= money) return money - (prices[0] + prices[1]);
+        return money;
+    }
+};
