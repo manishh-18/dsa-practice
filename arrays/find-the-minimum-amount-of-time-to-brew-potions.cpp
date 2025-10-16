29ccb1d688b9f447bd905b1619f576c1d4fabc47
manishh-18 <manishpatsariya14@gmail.com>
2025-10-16 13:08:48 +0530

    Add soltuion of 'Find the Minimum Amount of Time to Brew Potions' problem in arrays

---
+++ arrays/find-the-minimum-amount-of-time-to-brew-potions.cpp
@@ -0,0 +1,24 @@
+// Problem: Find the Minimum Amount of Time to Brew Potions (LeetCode 3494)
+// URL: https://leetcode.com/problems/find-the-minimum-amount-of-time-to-brew-potions/
+// Difficulty: Medium
+
+
+
+class Solution {
+public:
+    long long minTime(vector<int>& skill, vector<int>& mana) {
+        int n = skill.size(), m = mana.size();
+        vector<long long> done(n + 1, 0);
+        
+        for (int j = 0; j < m; ++j) {
+            for (int i = 0; i < n; ++i) {
+                done[i + 1] = max(done[i + 1], done[i]) + 1LL * mana[j] * skill[i];
+            }
+            for (int i = n - 1; i > 0; --i) {
+                done[i] = done[i + 1] - 1LL * mana[j] * skill[i];
+            }
+        }
+        
+        return done[n];
+    }
+};
