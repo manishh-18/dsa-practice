d5c6f9dd0ca39c9e53a5847975d75882af90ce15
manishh-18 <manishpatsariya14@gmail.com>
2025-10-23 09:48:15 +0530

    Add soltuion of 'Find the maximum achievable number' problem in maths

---
+++ maths/find-the-maximum-achievable-number.cpp
@@ -0,0 +1,11 @@
+// Problem: Find the maximum achievable number (LeetCode #2769)
+// Link: https://leetcode.com/problems/find-the-maximum-achievable-number/
+// Difficulty: Easy
+
+
+class Solution {
+public:
+    int theMaximumAchievableX(int num, int t) {
+        return num + 2 * t;
+    }
+};
