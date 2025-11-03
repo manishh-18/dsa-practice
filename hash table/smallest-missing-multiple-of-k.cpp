788f76e64d0e20f386553f52acb72243c9f8ebd0
manishh-18 <manishpatsariya14@gmail.com>
2025-11-03 21:56:52 +0530

    Updated problem solved count in Readme

---
+++ hash table/smallest-missing-multiple-of-k.cpp
@@ -0,0 +1,15 @@
+// Problem : Smallest Missing Multiple of K(LeetCode #3718)
+// URL     : https://leetcode.com/problems/smallest-missing-multiple-of-k/
+// Difficulty : Medium
+
+class Solution {
+public:
+    int missingMultiple(vector<int>& nums, int k) {
+        unordered_set<int>s(nums.begin(),nums.end());
+        int x = k;
+        while(true){
+            if(s.find(x) == s.end()) return x;
+            x += k;
+        }
+    }
+};
