66615d8421b4f398cb20568f67927dbd17265ab7
manishh-18 <manishpatsariya14@gmail.com>
2025-11-07 18:00:03 +0530

    Add soltuion of 'Make Array Elements Equal to Zero' problem in arrays

---
+++ arrays/make-array-elements-equal-to-zero.cpp
@@ -0,0 +1,26 @@
+// Problem: Make Array Elements Equal to Zero (LeetCode #3354)
+// Link: https://leetcode.com/problems/make-array-elements-equal-to-zero/
+// Difficulty: Easy
+
+class Solution {
+public:
+    int countValidSelections(vector<int>& nums) {
+        int count = 0, n = nums.size();
+        int sum = 0, halfSum = 0;
+
+        for (int num : nums) sum += num;
+
+        for (int i = 0; i < n; i++) {
+            halfSum += nums[i];
+            if (nums[i] == 0) {
+                if (2 * halfSum == sum) {
+                    count += 2;
+                } else if (abs(sum - 2 * halfSum) == 1) {
+                    count++;
+                } 
+            }
+        }
+        
+        return count;
+    }
+};
