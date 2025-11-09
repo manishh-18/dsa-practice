7dc9149af02a93140bfe0071db8342f3dba73dc6
manishh-18 <manishpatsariya14@gmail.com>
2025-11-09 11:44:48 +0530

    Add soltuion of 'Find the Peaks' problem in arrays

---
+++ arrays/find-the-peaks.cpp
@@ -0,0 +1,16 @@
+// Problem: Find the Peaks (LeetCode #2951)
+// Link: https://leetcode.com/problems/find-the-peaks/
+// Difficulty: Easy
+
+
+class Solution {
+public:
+    vector<int> findPeaks(vector<int>& mountain) {
+        vector<int> ans;
+        for (int i = 1; i < mountain.size() - 1; i++) {
+            if (mountain[i - 1] < mountain[i] && mountain[i] > mountain[i + 1])
+                ans.push_back(i);
+        }
+        return ans;
+    }
+};
